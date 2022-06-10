b11de34c3806a3a24e1b8875682c28dbbb22ab24
werthmannsupercode <werthmannsupercode@gmail.com>
2022-06-10 12:29:49 +0200

    first try

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [answer, setAnswer] = useState(true)
+  const [answer2, setAnswer2] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container">
+      <div className="innerContainer">
+        <p>Why ist React great?</p>
+        <button onClick={() => setAnswer(!answer)}>+</button>
+      </div>
+      <div style={answer ? { display: 'none' } : { display: 'flex' }} className="innerContainer">
+        <p className="answerP">Fast Learning Curve</p>
+        <button onClick={() => setAnswer2(!answer2)}>+</button>
+      </div>
+      <div style={answer2 ? { display: 'none' } : { display: 'block' }} >
+        <p className="answer2P">React is very a simple and lightweight library that only deals with the view layer. It is not a beast like other MV* frameworks such as Angular or Ember. Any Javascript developer can understand the basics and start developing an awesome web application after only a couple of days reading tutorial.
+          As the React guide says ‘Thinking in React’ may be a little different than you used to since it brings a new approach to the table, but it will become much easier and natural as you gain experience with it.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </div >
   );
 }
 
